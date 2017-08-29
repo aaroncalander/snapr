c5868cc6067cb5162b3df0ebf46bf08d0929c937
Aaron Calander <aaroncalander@yahoo.com>
2017-08-29 16:05:43 -0700

    Adds additional validation so only owner of a post can edit or delete it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :owned_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -57,5 +58,12 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "Step off, that isn't your post, baby!"
+      redirect_to root_path
+    end
   end
 end
